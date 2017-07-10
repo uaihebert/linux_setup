2706621c171d9677cb952b7e602895f363a4714c
Hebert Coelho <uaihebert@gmail.com>
2017-07-10 19:14:43 +0200

    updating configs for kuber/linux

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# linux_setup
+# Personal configs to carry arround the pcs.
+
+The following var are needed so far:
+* KUBERNET_DEFAULT_NAMESPACE
+* DEV_WORKSPACE
+
+( :
--- alias_linux.sh
+++ alias_linux.sh
@@ -1,7 +1,7 @@
 alias ll="ls -lha"
 alias fin="find . -iname "
 alias psg="ps aux | grep"
-alias cdw="cd ~/dev/workspace"
+alias cdw='cd "$DEV_WORKSPACE"'
 alias grin='_grin(){grep -rin --exclude-dir=".idea" "$1" .}; _grin'
 alias ltrun='gtruncate --size 0 log/*'
 

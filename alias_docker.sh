fca9fbfa44e6ad377fd3a582868aa518b0f136d8
Hebert Coelho <uaihebert@gmail.com>
2017-10-05 09:32:56 +0200

    Creating alias for dbs and angular

--- alias_docker.sh
+++ alias_docker.sh
@@ -11,7 +11,27 @@ alias pg_start="docker run --name local-postgres9.6.5 -p 5432:5432 -e POSTGRES_P
 alias pg_stop="docker stop local-postgres9.6.5 && pg_remove"
 alias pg_remove="docker rm -v local-postgres9.6.5"
 alias psql="run_psql"
+alias pg_bash="docker exec -it local-postgres9.6.5 bash"
 
 function run_psql {
 docker run -it --link local-postgres9.6.5:postgres --rm postgres:9.6.5 sh -c 'PGPASSWORD=asecurepassword exec psql -h "$POSTGRES_PORT_5432_TCP_ADDR" -p "$POSTGRES_PORT_5432_TCP_PORT" -U postgres'
 }
+
+# MariaDB
+alias maria_vol_create="docker create -v /var/lib/mariadb/data --name mariadb_volume mariadb"
+alias maria_start="docker run --name mariadb_server -d -p 3306:3306 --volumes-from mariadb_volume -e MYSQL_ROOT_PASSWORD=secret mariadb"
+alias mariadb="run_mariadb"
+alias maria_stop="docker stop mariadb_server && docker rm -v mariadb_server"
+alias mysql="mariadb"
+alias maria_bash="docker exec -it mariadb_server bash"
+
+function run_mariadb {
+docker run -it --link mariadb_server:mysql --rm mariadb sh -c 'exec mysql -h"$MYSQL_PORT_3306_TCP_ADDR" -P"$MYSQL_PORT_3306_TCP_PORT" -uroot -p"$MYSQL_ENV_MYSQL_ROOT_PASSWORD"'
+}
+
+# Mongo
+alias mongo_vol_create="docker create -v /var/lib/mongo/data --name mongo-data mongo"
+alias mongo_start="docker run --name mongo_db -d -p 27017:27017 -d --volumes-from mongo-data mongo"
+alias mongo_stop="docker stop mongo_db && docker rm -v mongo_db"
+alias mongo="docker exec -i -t mongo_db mongo"
+alias mongo_bash="docker exec -it maria_db bash"
--- alias_linux.sh
+++ alias_linux.sh
@@ -1,8 +1,12 @@
 alias ll="ls -lha"
 alias fin="find . -iname "
 alias psg="ps aux | grep"
+alias dev='cd ~/dev'
 alias cdw='cd "$DEV_WORKSPACE"'
 alias grin='_grin(){grep -rin --exclude-dir=".idea" "$1" .}; _grin'
 alias ltrun='gtruncate --size 0 log/*'
 alias tex='open -a TextEdit'
 alias sublime='/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl'
+alias vpi='_vpi() {cd ~/.vim/bundle;git clone "$1";cd -}; _vpi'
+
+alias ngs='ng server'
